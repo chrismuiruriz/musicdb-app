7c96263f63442220959ad1ed339900f5ec3e5b13
chris muiruri <chrismuiruri007@gmail.com>
2022-01-15 23:34:29 +0300

    Initial commit: project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useParams,
+} from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search/:q" element={<SearchResults />} />
+        <Route path="/artist/:id" element={<Artist />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+//pages
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+
+      <Link to="/artist/123">Artist</Link>
+      <Link to={`/search/${encodeURI("Who is there?")}`}>Artist</Link>
+    </div>
+  );
+}
+
+function SearchResults() {
+  let { q } = useParams();
+
+  return (
+    <div>
+      <h2>Search Results for {q}</h2>
+    </div>
+  );
+}
+
+function Artist() {
+  let { id } = useParams();
+  return (
+    <div>
+      <h2>Artist {id}</h2>
     </div>
   );
 }
